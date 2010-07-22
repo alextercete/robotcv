2d0ccbcedbccba62942d28124f7a1b3972014902
Alex Tercete <alextercete@gmail.com>
2010-07-22 01:12:53 -0300

    Started to send the robots positions through the serial port using pySerial.

--- message_formatter.py
+++ message_formatter.py
@@ -1,7 +1,11 @@
 import struct
 
-SEND_ROBOTS_POSITIONS = 1
+# Start and end of transmission
+SOT = chr(0x01)
+EOT = chr(0x02)
 
+# Messages
+SEND_ROBOTS_POSITIONS = chr(0x19)
 
 class BadFormatError(Exception):
 
@@ -15,38 +19,49 @@ class BadFormatError(Exception):
 class MessageFormatter:
 
     @classmethod
-    def encode(cls, message_type, message):
-        formats = cls.get_encoding_formats(message_type, message)
-        return struct.pack(formats, *message)
+    def encode(cls, message_type, data):
+        encoded_data = cls.encode_data(message_type, data)
+        message_length = chr(len(message_type + encoded_data))
+
+        return '{0}{1}{2}{3}{4}'.format(SOT,
+                                        message_length,
+                                        message_type,
+                                        encoded_data,
+                                        EOT)
 
     @classmethod
-    def decode(cls, message_type, message):
-        formats = cls.get_decoding_formats(message_type, message)
-        return struct.unpack(formats, message)
+    def encode_data(cls, message_type, data):
+        formats = cls.get_encoding_formats(message_type, data)
+        return struct.pack(formats, *data)
 
     @classmethod
-    def get_encoding_formats(cls, message_type, message):
+    def decode_data(cls, message_type, data):
+        formats = cls.get_decoding_formats(message_type, data)
+        return struct.unpack(formats, data)
+
+    @classmethod
+    def get_encoding_formats(cls, message_type, data):
 
         basic_format = {
-            SEND_ROBOTS_POSITIONS: 'chh',
+            SEND_ROBOTS_POSITIONS: 'hh',
         }[message_type]
 
-        return cls.get_formats(basic_format, len(basic_format), len(message))
+        return cls.get_formats(basic_format, len(basic_format), len(data))
 
     @classmethod
-    def get_decoding_formats(cls, message_type, message):
+    def get_decoding_formats(cls, message_type, data):
 
         basic_format, minimum_length = {
-            SEND_ROBOTS_POSITIONS: ('chh', 5),
+            SEND_ROBOTS_POSITIONS: ('hh', 4),
         }[message_type]
 
-        return cls.get_formats(basic_format, minimum_length, len(message))
+        return cls.get_formats(basic_format, minimum_length, len(data))
 
     @classmethod
-    def get_formats(cls, basic_format, minimum_length, message_length):
+    def get_formats(cls, basic_format, minimum_length, data_length):
 
-        if message_length % minimum_length > 0:
+        if data_length % minimum_length > 0:
             raise BadFormatError('Message length ({0}) is not multiple of {1}!'\
-                                     .format(message_length, minimum_length))
+                                     .format(data_length, minimum_length))
 
-        return '=' + basic_format * int(message_length / minimum_length)
+        return '=' + basic_format * int(data_length / minimum_length)
